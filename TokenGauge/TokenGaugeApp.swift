a4787318d8db8cd137a023e4311e43eb490d8538
l2juhan <leejuhan0527@gmail.com>
2026-03-06 21:10:54 +0900

    Initial Commit

---
+++ TokenGauge/TokenGaugeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokenGaugeApp.swift
+//  TokenGauge
+//
+//  Created by 이주한 on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct TokenGaugeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TokenGaugeTests/TokenGaugeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TokenGaugeTests.swift
+//  TokenGaugeTests
+//
+//  Created by 이주한 on 3/6/26.
+//
+
+import Testing
+@testable import TokenGauge
+
+struct TokenGaugeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
